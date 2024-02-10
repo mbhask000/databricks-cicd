6ea4e53e7e7ad8c2f843ee973be007b332690895
gchandra10 <gchandra10>
2024-02-10 22:53:31 +0000

    Making Unit test fail

--- test_people_pipeline.py
+++ test_people_pipeline.py
@@ -1,12 +1,12 @@
 # Databricks notebook source
 def test_gold_output():
-    assert spark.table("gold_people1"), "Table Not Found"
+    assert spark.table("gold_people"), "Table Not Found"
 
 # COMMAND ----------
 
 def test_columns():
     cols_to_check = ['gender','total_salary','gender_count']
-    assert spark.table("gold_people1").columns == cols_to_check, "Column not matched"
+    assert spark.table("gold_people").columns == cols_to_check, "Column not matched"
 
     
 
@@ -15,7 +15,7 @@ def test_columns():
 from pyspark.sql.functions import col
 
 def test_nulls_in_gold():
-    df = spark.table("gold_people1")
+    df = spark.table("gold_people")
     for column in ['gender','total_salary','gender_count']:
         null_count = df.filter(col(column).isNull()).count()
         assert null_count == 0, f"Column {column} contains {null_count} null values"
@@ -31,6 +31,10 @@ test_columns()
 # COMMAND ----------
 
 test_nulls_in_gold()
+
+# COMMAND ----------
+
+
 
 # COMMAND ----------
 
